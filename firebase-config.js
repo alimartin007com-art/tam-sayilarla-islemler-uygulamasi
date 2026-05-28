dce42963f9343eba15ff4aa2aa187a62e99134b2
alimartin007com-art <alimartin007.com@gmail.com>
2026-05-28 20:48:16 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// --- FIREBASE CONFIGURATION ---
+// Bu dosya hassas API anahtarlarını barındırır.
+// Git repolarında paylaşılmaması için .gitignore dosyasına eklenmiştir.
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCR2LWyzcoT2wPwGzS3oF47RnNx6LxZbHE",
+  authDomain: "eglenceli-matematik-oyun-f2396.firebaseapp.com",
+  databaseURL: "https://eglenceli-matematik-oyun-f2396-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "eglenceli-matematik-oyun-f2396",
+  storageBucket: "eglenceli-matematik-oyun-f2396.firebasestorage.app",
+  messagingSenderId: "803199070316",
+  appId: "1:803199070316:web:c56ce976770efee1b5e849",
+  measurementId: "G-E5NFXTYYT8"
+};
